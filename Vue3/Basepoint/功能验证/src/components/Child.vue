907b220b9675ec27b895ad777e5b6882b8318f7a
yuluo <2216847528@qq.com>
2024-09-03 08:55:44 +0800

    update(add Vue3 flor)

---
+++ Vue3/Basepoint/功能验证/src/components/Child.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>Child</div>
+    <p @click="setCount(50)">count:{{ count }}</p>
+</template>
+
+<script setup lang="ts">
+import { ref, defineExpose } from "vue";
+
+let a = 1;
+let count = ref(100);
+let setCount = function (n: number) {
+    count.value = n;
+}
+// 如果要在组件外部使用当前组件的数据状态，必须暴露。
+defineExpose({ a, count, setCount })
+</script>
+
+<style scoped></style>
---
+++ Vue3/Basepoint/功能验证/vue.config.js
@@ -0,0 +1,20 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+    transpileDependencies: true,
+    devServer: {
+        open: true,// 自动打开浏览器
+        port: 8080,// 指定端口号
+        host: "127.0.0.1",// 指定host
+    },
+    // 关闭编译时的特征标志
+    chainWebpack: (config) => {
+        config.plugin('define').tap((definitions) => {
+            Object.assign(definitions[0], {
+                __VUE_OPTIONS_API__: 'true',
+                __VUE_PROD_DEVTOOLS__: 'false',
+                __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'false'
+            })
+            return definitions
+        })
+    }
+})
