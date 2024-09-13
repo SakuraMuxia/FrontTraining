fe3d31a173cb76f976ca5d22156d5df54860a5b7
hanser <2216847528@qq.com>
2024-09-13 23:41:00 +0800

    update(react testing defaultProps)

--- React/BasePoint/react_cli/FunctionTesting/src/components/Class/Child.jsx
+++ React/BasePoint/react_cli/FunctionTesting/src/components/Class/Child.jsx
@@ -1,9 +1,24 @@
 import React, { Component } from 'react'
-
+// 1. 引入属性检查的包
+import PropTypes from 'prop-types'
 export default class Child extends Component {
-  render() {
-    return (
-      <div>Child</div>
-    )
-  }
+    // 定义静态属性
+    static propTypes = {
+        name: PropTypes.string.isRequired, // name是字符串，且必须传
+        age: PropTypes.number // age 是数字，可以不传
+    }
+    // 定义静态方法
+    static defaultProps = {
+        age: 100 // 限定默认值
+    }    
+    render() {
+        let { name, age } = this.props;
+        return (
+            <div>
+                <h3>Child</h3>
+                <p>props name: {name}</p>
+                <p>props age: {age}</p>
+            </div>
+        )
+    }
 }
--- React/BasePoint/react_cli/FunctionTesting/src/components/Fun/Child.jsx
+++ React/BasePoint/react_cli/FunctionTesting/src/components/Fun/Child.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
-
-export default function Child() {
+import PropTypes from 'prop-types'
+function Child({ name, age }) {
   return (
-    <div>Child</div>
+    <div>
+          <h4>函数组件</h4>
+          <p>props name: {name}</p>
+          <p>props age: {age}</p>
+    </div>
   )
 }
+// 通过给函数对象设置propTypes属性的方式设置类型
+Child.propTypes = {
+    name: PropTypes.string.isRequired,
+    age: PropTypes.number
+}
+// 通过给函数对象设置defaultProps属性的方式设置默认值
+Child.defaultProps = {
+    age: 10000
+}
+export default Child
