f9561df793a1fdf60df13a9798439a985eb581e0
hanser <2216847528@qq.com>
2024-09-23 23:52:51 +0800

    [更新] useRef 的初始值使用

--- React/BasePoint/react_cli/FunctionTesting/src/App.jsx
+++ React/BasePoint/react_cli/FunctionTesting/src/App.jsx
@@ -1,11 +1,26 @@
 // 1. 从react中解构 useEffect
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
-export default function Timer() {
+export default function App() {
+
+    let [count, setCount] = useState(88)
+    const flagRef = useRef(true)
+    console.log(flagRef) //当初次调用代码时，执行到这 {current:true}
     
+    useEffect(()=>{
+        console.log("App is mouted")
+        if (flagRef.current) {
+            flagRef.current = false; // 初次挂载完成后 {current:false}
+            return; // 初次挂载完成后,直接跳出函数
+        }
+        console.log("App is update")
+    })
     return (
         <div>
-            <p>当前日期是: {nowDate}</p>
+            <p>{count}</p>
+            <p><button onClick={() => {
+                setCount(count + 1)
+            }}>count++</button></p>
         </div>
     )
 }
