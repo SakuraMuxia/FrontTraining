2ea84e88f448c258c8cef048b50f07a220210e4f
yuluo <2216847528@qq.com>
2024-09-26 16:26:14 +0800

    :sparkles: React路由组件传参，嵌套路由，路由重定向，路由懒加载练习

--- React/BasePoint/react_cli/FunctionTesting/src/App.jsx
+++ React/BasePoint/react_cli/FunctionTesting/src/App.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
-import './App.css'
-import Header from './components/Header'
-import Main from './components/Main'
+
 
 export default function App() {
+
     return (
-        <div className="container">
-            <Header />
-            <Main />
+        <div>
+            
         </div>
     )
 }
--- React/BasePoint/react_cli/FunctionTesting/src/content/content.js
+++
@@ -1,7 +0,0 @@
-// 1 导入createContext 函数
-import { createContext } from 'react'
-// 2 创建context对象
-const context = createContext();
-
-// 3. 将context对象暴露出去
-export default context;
