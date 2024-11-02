abfb4e55e405c2a9c625be368e3984648e920fde
yuluo <2216847528@qq.com>
2024-11-02 11:34:11 +0800

    ✅ 新增品牌管理页面
    - 新增品牌接口,数据渲染,分页,
    - 新增Dialog对话框表单静态页面渲染
    - 新增更新品牌交互和操作
    - 新增删除品牌功能
    - 导出Excel表格功能(续)

--- Vue3/Project/backEndManagement/src/api/product/trademark.ts
+++ Vue3/Project/backEndManagement/src/api/product/trademark.ts
@@ -1,10 +1,31 @@
 import request from '@/utils/request'
-import type { TradeMarkResponseData } from './type/trademark'
+import type { TradeMarkResponseData,TradeMark } from './type/trademark'
 
 // 枚举地址
 enum API {
-    GETTRADEMARK_URL = '/admin/product/baseTrademark/'
+    //获取已有的品牌
+    GETTRADEMARK_URL = '/admin/product/baseTrademark/',
+    //添加新增的品牌接口:post  {tmName:'xxx',logoUrl:'xxx'}
+    ADDTRADEMARK_URL = "/admin/product/baseTrademark/save",
+    //更新已有的品牌  post  {id:'zzz',tmName:'xxx',logoUrl:'xxx'}
+    UPDATETRADEMARK_URL = "/admin/product/baseTrademark/update",
+    //删除已有的品牌数据
+    DLETETRADENARK_URL = "/admin/product/baseTrademark/remove/"
+
 }
 
 // 获取品牌列表数据 
 export const reqTradeMark = (page:number,limit:number) => request.get<any,TradeMarkResponseData>(API.GETTRADEMARK_URL+`${page}/${limit}`)
+
+//添加与更新品牌
+export const reqAddOrUpdateTradeMark = (data: TradeMark) => {
+    //更新已有的品牌
+    if (data.id) {
+        return request.put<any, any>(API.UPDATETRADEMARK_URL, data);
+    } else {
+        //新增品牌
+        return request.post<any, any>(API.ADDTRADEMARK_URL, data);
+    }
+}
+// 删除品牌的接口
+export const reqDeleteById = (id: number) => request.delete<any, any>(API.DLETETRADENARK_URL + id);
