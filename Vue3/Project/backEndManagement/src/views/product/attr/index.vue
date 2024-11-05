c0a3f7fddc9e672d8fd562359a2a34d84f2ffe12
yuluo <2216847528@qq.com>
2024-11-05 20:28:39 +0800

    ✅ 更新属性页面
    - 更新属性页面静态页面
    - 新增添加属性,封装接口,编辑与查看切换
    - 保存按钮禁用时机
    - 删除按钮

--- Vue3/Project/backEndManagement/src/views/product/attr/index.vue
+++ Vue3/Project/backEndManagement/src/views/product/attr/index.vue
@@ -55,27 +55,32 @@
                         <el-input
                             type="text"
                             placeholder="请你输入属性名称"
+                            v-model="attrParams.attrName"
                         ></el-input>
                     </el-form-item>
                 </el-form>
-                <el-button type="primary" :icon="Plus">添加属性值</el-button>
+                <el-button type="primary" :icon="Plus" @click="addAttrValue" :disabled="attrParams.attrName ? false : true">添加属性值</el-button>
                 <el-button @click="scene = 0">取消</el-button>
-                <el-table border style="margin: 10px 0px" >
-
+                <el-table border style="margin: 10px 0px" :data="attrParams.attrValueList">
                     <el-table-column
                         type="index"
                         label="序号"
                         align="center"
                         width="80px"
                     ></el-table-column>
-
                     <el-table-column label="属性值">
                         <!-- row:即为每一个属性值对象 -->
                         <template #="{ row, $index }">
+                            <!-- input标签 -->
                             <el-input
                             ref="inputRef"
                             size="small"
+                            v-if="row.showInput"
+                            v-model="row.valueName"
+                            @blur="toLook(row, $index)"
                             ></el-input>
+                            <!-- div标签 -->
+                            <div v-else @click="toEdit(row)">{{ row.valueName }}</div>
                         </template>
                     </el-table-column>
 
@@ -85,12 +90,15 @@
                             type="danger"
                             :icon="Delete"
                             size="small"
+                            @click="attrParams.attrValueList.splice($index, 1)"
                             ></el-button>
                         </template>
                     </el-table-column>
                 </el-table>
                 <el-button
                     type="primary"
+                    @click="save"
+                    :disabled="attrParams.attrName && attrParams.attrValueList.length ? false : true"
                     >保存</el-button>
                 <el-button @click="scene = 0">取消</el-button>
             </div>
@@ -99,8 +107,8 @@
 </template>
 
 <script setup lang='ts'>
-import { reqAttrList } from "@/api/product/attr";
-import type { AttrResponseData } from "@/api/product/type/attr";
+import { reqAddOrUpdateAttr, reqAttrList } from "@/api/product/attr";
+import type { AttrResponseData,Attr, AttrValue } from "@/api/product/type/attr";
 import useCategoryStore from "@/stores/category";
 import { Plus, Edit, Delete } from "@element-plus/icons-vue";
 import { ElMessage } from "element-plus";
@@ -108,6 +116,15 @@ import { watch, ref, reactive, nextTick } from "vue";
 
 // 属性列表
 const attrList = ref<AttrResponseData>([])
+
+// 添加属性和属性值，请求体
+const attrParams = reactive<Attr>({
+    categoryId: "", //新增的属性归属于哪一个三级分类
+    categoryLevel: "3", //代表几级分类，默认3级
+    attrName: "", //属性的名字
+    //属性值数组
+    attrValueList: [],
+})
 
 // 定义分类小仓库(分类组件的)
 const categoryStore = useCategoryStore();
@@ -129,7 +146,6 @@ watch(()=>categoryStore.c3Id,()=>{
     categoryStore.c3Id && getAttrList();
 })
 
-// 方法
 //获取全部已有的属性与属性值方法
 const getAttrList = async () => {
     //一级、二级、三级分类的ID
@@ -138,12 +154,130 @@ const getAttrList = async () => {
     attrList.value = result;
 }
 
+// 添加属性
 const addAttr = () =>{
+    //清空数据
+    reset();
+    //收集三级分类的ID[也可以点击保存的时候收集]
+    attrParams.categoryId = categoryStore.c3Id;
     // 设置场景为添加属性
     scene.value = 1;
 }
 
-// //用户取消、保存成功以后要清空数据的方法
+// 添加属性值
+const addAttrValue =()=>{
+    //向属性值数组添加属性对象
+    //在每一个属性值对象身上放置编辑与查看模式标记
+    attrParams.attrValueList.push({
+        valueName: "",
+        showInput: true,
+    })
+    //当响应式数据发生变化后,立即调用nextTick方法,获取更新后的DOM|组件实例
+    nextTick(() => {
+        inputRef.value.focus();
+    });
+}
+
+// 提交
+const save = async() =>{
+    try {
+        await reqAddOrUpdateAttr(attrParams)
+        //消息提示
+        ElMessage({
+            type: "success",
+            message: attrParams.id ? "更新成功" : "添加成功",
+        });
+        //切换场景为0
+        scene.value = 0;
+        //添加或者更新成功以后再次获取全部已有的属性
+        getAttrList();
+    } catch (error) {
+        //消息提示
+        ElMessage({
+            type: "error",
+            message: attrParams.id ? "更新失败" : "添加失败",
+        });
+    }
+    
+}
+
+// 用户取消、保存成功以后要清空数据的方法
+const reset = () => {
+  //ES6的语法:Object.assign
+  Object.assign(attrParams, {
+    categoryId: "", //新增的属性归属于哪一个三级分类
+    categoryLevel: "3", //代表几级分类
+    attrName: "", //属性的名字
+    //属性值数组
+    attrValueList: [],
+    // 更新时
+    id: "",
+  });
+};
+
+// input失去焦点
+const toLook = (row:AttrValue,index:number) => {
+
+    //属性值不能为空
+    if (row.valueName.trim() == "") {
+        ElMessage({
+            type: "error",
+            message: "属性值不能为空",
+        });
+        //从数组中删除当前元素
+        attrParams.attrValueList.splice(index, 1);
+        //属性值为空，后面的语句不在执行,div不出现
+        return;
+    }
+
+    //判断属性值不能重复
+    const repeat = attrParams.attrValueList.find((item) => {
+        //把除自己以外的元素都返回
+        if (row != item) {
+        return row.valueName.trim() === item.valueName.trim();
+        }
+    })
+    if (repeat) {
+        ElMessage({
+        type: "error",
+        message: "属性值不能重复",
+        });
+        //从数组中删除当前元素
+        attrParams.attrValueList.splice(index, 1);
+        return;
+    }
+
+    //显示div
+    row.showInput = false
+}
+
+//div点击事件
+const toEdit = (row: AttrValue) => {
+    //点击div 变为 input
+    row.showInput = true;
+    // input输入框获取焦点
+    nextTick(() => {
+        inputRef.value.focus();
+    });
+};
+
+//删除已有的属性
+const deleteAttr = async (id: number) => {
+    try {
+        //删除已有属性成功
+        await reqDeleteAttr(id);
+        //消息提示
+        ElMessage({
+            type:'success',
+            message:'删除成功'
+        });
+        //再次获取最新剩下全部属性
+        getAttrList();
+    } catch (error) {
+
+    }
+}
+
 
 
 </script>
