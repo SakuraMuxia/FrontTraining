5589561338c4c6a12fdf48dab9bc78be843c131c
hanser <2216847528@qq.com>
2024-11-13 20:11:51 +0800

    ✅ 更新用户菜单权限控制
    - 完成不同用户登陆,菜单路由权限不同

--- Vue3/Project/backEndManagement/src/router/routes.ts
+++ Vue3/Project/backEndManagement/src/router/routes.ts
@@ -2,17 +2,19 @@ import type { RouteRecordRaw } from 'vue-router';
 
 //静态路由:常量路由
 export const staticRoutes: Array<RouteRecordRaw> = [
+	//登录
 	{
-		// 路径全是小写的
 		path: '/login',
-		name: 'Login',
 		component: () => import('@/views/login/index.vue'),
-		// 路由元数据
+		name: 'Login',
 		meta: {
-			hidden: true
+			//hidden:决定当前路由是否在左侧菜单展示 true:隐藏 false:显示
+			//title:决定路由在左侧展示标题
+			//icon:左侧菜单文字前面的图标
+			hidden: true,
 		}
 	},
-
+	//404 
 	{
 		path: '/404',
 		name: '404',
@@ -21,7 +23,7 @@ export const staticRoutes: Array<RouteRecordRaw> = [
 			hidden: true
 		}
 	},
-
+	// 首页
 	{
 		path: '/',
 		component: () => import('@/layout/index.vue'),
@@ -36,56 +38,11 @@ export const staticRoutes: Array<RouteRecordRaw> = [
 			}
 		}]
 	},
-	
-	{
-		path: '/product', // 一级路由展示 layout组件
-		component: () => import('@/layout/index.vue'),
-		name: 'Product',
-		meta: {
-			title: '商品管理',
-			icon: 'ele-Goods'
-		},
-		children: [
-			{
-				path: 'trademark',
-				component: () => import('@/views/product/trademark/index.vue'),
-				name: 'Trademark',
-				meta: {
-					title: '品牌管理',
-					icon: 'ele-Apple'
-				}
-			},
-			{
-				path: 'attr',
-				component: () => import('@/views/product/attr/index.vue'),
-				name: 'Attr',
-				meta: {
-					title: '属性管理',
-					icon: 'ele-IceTea'
-				}
-			},
-			{
-				path: 'spu',
-				component: () => import('@/views/product/spu/index.vue'),
-				name: 'Spu',
-				meta: {
-					title: 'SPU管理',
-					icon: 'ele-Burger'
-				}
-			},
-			{
-				path: 'sku',
-				component: () => import('@/views/product/sku/index.vue'),
-				name: 'Sku',
-				meta: {
-					title: 'SKU管理',
-					icon: 'ele-Goblet'
-				}
-			}
-		]
-	},
+];
 
-	// 权限管理 一级路由
+//存储异步路由
+export const asyncRoute = [
+	// 权限管理
 	{
 		name: 'Acl',
 		path: '/acl',
@@ -94,8 +51,8 @@ export const staticRoutes: Array<RouteRecordRaw> = [
 		meta: { title: '权限管理', icon: 'ele-Setting' },
 		children: [
 			{
-				path: '/acl/user/list',
 				name: 'User',
+				path: '/acl/user/list',
 				component: () => import('@/views/acl/user/index.vue'),
 				meta: { title: '用户管理' }
 			},
@@ -118,8 +75,8 @@ export const staticRoutes: Array<RouteRecordRaw> = [
 				},
 			},
 			{
-				name: 'Menu',
-				path: '/acl/menu/list',
+				name: 'Permission',
+				path: '/acl/permission/list',
 				component: () => import('@/views/acl/menu/index.vue'),
 				meta: {
 					title: '菜单管理',
@@ -127,123 +84,66 @@ export const staticRoutes: Array<RouteRecordRaw> = [
 			}
 		]
 	},
-
-	/* 匹配任意的路由 必须最后注册 */
+	//商品管理模块的路由
 	{
-		path: '/:pathMatch(.*)',
-		name: 'Any',
-		redirect: '/404',
+		path: '/product',
+		component: () => import('@/layout/index.vue'),
+		name: 'Product',
 		meta: {
-			hidden: true
-		}
+			title: '商品管理',//菜单的标题
+			icon: "ele-Goods"
+		},
+		children: [
+			{
+				path: 'trademark',
+				component: () => import('@/views/product/trademark/index.vue'),
+				name: 'Trademark',
+				meta: {
+					title: '品牌管理',
+					icon: 'ele-Operation'
+				}
+			},
+			{
+				path: 'attr',
+				component: () => import('@/views/product/attr/index.vue'),
+				name: 'Attr',
+				meta: {
+					title: '属性管理',
+					icon: 'ele-Files'
+				}
+			},
+			{
+				path: 'spu',
+				component: () => import('@/views/product/spu/index.vue'),
+				name: 'Spu',
+				meta: {
+					title: 'Spu管理',
+					icon: 'ele-Platform'
+				}
+			}
+			,
+			{
+				path: 'sku',
+				component: () => import('@/views/product/sku/index.vue'),
+				name: 'Sku',
+				meta: {
+					title: 'sku管理',
+					icon: 'ele-Football'
+				}
+			}
+
+		]
+
 	}
-	
-];
+]
 
-// // 定义动态路由
-// export const asyncRoutes: Array<RouteRecordRaw> = [
-// 	// 商品管理模块 一级路由
-// 	{
-// 		path: '/product', // 一级路由展示 layout组件
-// 		component: () => import('@/layout/index.vue'),
-// 		name: 'Product',
-// 		meta: {
-// 			title: '商品管理',
-// 			icon: 'ele-Goods'
-// 		},
-// 		children: [
-// 			{
-// 				path: 'trademark',
-// 				component: () => import('@/views/product/trademark/index.vue'),
-// 				name: 'Trademark',
-// 				meta: {
-// 					title: '品牌管理',
-// 					icon: 'ele-Apple'
-// 				}
-// 			},
-// 			{
-// 				path: 'attr',
-// 				component: () => import('@/views/product/attr/index.vue'),
-// 				name: 'Attr',
-// 				meta: {
-// 					title: '属性管理',
-// 					icon: 'ele-IceTea'
-// 				}
-// 			},
-// 			{
-// 				path: 'spu',
-// 				component: () => import('@/views/product/spu/index.vue'),
-// 				name: 'Spu',
-// 				meta: {
-// 					title: 'SPU管理',
-// 					icon: 'ele-Burger'
-// 				}
-// 			},
-// 			{
-// 				path: 'sku',
-// 				component: () => import('@/views/product/sku/index.vue'),
-// 				name: 'Sku',
-// 				meta: {
-// 					title: 'SKU管理',
-// 					icon: 'ele-Goblet'
-// 				}
-// 			}
-// 		]
-// 	},
 
-// 	// 权限管理 一级路由
-// 	{
-// 		name: 'Acl',
-// 		path: '/acl',
-// 		component: () => import('@/layout/index.vue'),
-// 		redirect: '/acl/user/list',
-// 		meta: { title: '权限管理', icon: 'ele-Setting' },
-// 		children: [
-// 			{
-// 				name: 'User',
-// 				path: '/acl/user/list',
-// 				component: () => import('@/views/acl/user/index.vue'),
-// 				meta: { title: '用户管理' }
-// 			},
-// 			{
-// 				name: 'Role',
-// 				path: '/acl/role/list',
-// 				component: () => import('@/views/acl/role/index.vue'),
-// 				meta: {
-// 				title: '角色管理',
-// 				},
-// 			},
-// 			{
-// 				name: 'RoleAuth',
-// 				path: '/acl/role/auth',
-// 				component: () => import('@/views/acl/role/roleAuth.vue'),
-// 				meta: {
-// 				title: '角色管理',
-// 				hidden: true,
-// 				activeMenu: '/acl/role/list',
-// 				},
-// 			},
-// 			{
-// 				name: 'Permission',
-// 				path: '/acl/permission/list',
-// 				component: () => import('@/views/acl/permission/index.vue'),
-// 				meta: {
-// 				title: '菜单管理',
-// 				}
-// 			}
-// 		]
-// 	},
-// ];
-
-// // 任意路由
-// export const anyRoute = [
-// 	/* 匹配任意的路由 必须最后注册 */
-// 	{
-// 		path: '/:pathMatch(.*)',
-// 		name: 'Any',
-// 		redirect: '/404',
-// 		meta: {
-// 			hidden: true
-// 		}
-// 	}
-// ]
+//存储任意路由
+export const anyRoute = {
+	path: '/:pathMatch(.*)',
+	name: 'Any',
+	redirect: '/404',
+	meta: {
+		hidden: true
+	}
+}
