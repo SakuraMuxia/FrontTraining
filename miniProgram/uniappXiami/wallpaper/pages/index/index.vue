c16967b89295e600cff27372b3eb49665aa0a4ba
yuluo <2216847528@qq.com>
2025-03-19 17:46:03 +0800

    ✨ 新增基于uniapp的xiami壁纸项目启动

---
+++ miniProgram/uniappXiami/wallpaper/pages/index/index.vue
@@ -0,0 +1,44 @@
+<template>
+	<view class="homeLayout pageBg">
+		<!-- 轮播图 -->
+		<view class="banner">
+			<swiper circular indicator-dots indicator-color="rgba(255,255,255,0.5)" indicator-active-color="#fff" autoplay>
+				<swiper-item>
+					<image src="../../common/images/banner1.jpg" mode="aspectFill"></image>
+				</swiper-item>
+				<swiper-item>
+					<image src="../../common/images/banner2.jpg" mode="aspectFill"></image>
+				</swiper-item>
+				<swiper-item>
+					<image src="../../common/images/banner3.jpg" mode="aspectFill"></image>
+				</swiper-item>
+			</swiper>
+		</view>
+	</view>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+.homeLayout {
+	.banner {
+		width: 750rpx;
+		// 设置图片的左右间隔
+		padding: 30rpx 0;
+		swiper {
+			width: 750rpx;
+			height: 340rpx;
+			&-item {
+				width: 100%;
+				height: 100%;
+				padding: 0 30rpx;
+				image {
+					width: 100%;
+					height: 100%;
+					border-radius: 10rpx;
+				}
+			}
+		}
+	}
+}
+</style>
